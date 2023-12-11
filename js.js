578dda8ff67e640ec5408db25133275c22a72b7b
swayam35 <152762218+swayam35@users.noreply.github.com>
2023-12-11 11:31:34 +0530

    update

--- js.js
+++ js.js
@@ -1,32 +1,17 @@
-/*Name this external file gallery.js*/
+/*Add the JavaScript here for the function billingFunction().  It is responsible for setting and clearing the fields in Billing Information */
 
-function upDate(previewPic){
-    /* In this function you should 
-       1) change the url for the background image of the div with the id = "image" 
-       to the source file of the preview image
-       
-       2) Change the text  of the div with the id = "image" 
-       to the alt text of the preview image 
-       */
-     var image = previewPic.src;
-      document.getElementById("image").style.backgroundImage = "url('" + image +"')";
-     
-      var text = previewPic.alt;
-     document.getElementById("image").innerHTML = text;
-     
-     
-       }
-   
-       function unDo(){
-        /* In this function you should 
-       1) Update the url for the background image of the div with the id = "image" 
-       back to the orginal-image.  You can use the css code to see what that original URL was
-       
-       2) Change the text  of the div with the id = "image" 
-       back to the original text.  You can use the html code to see what that original text was
-       */
-        document.getElementById("image").style.backgroundImage = "url('" + null +"')";
-       
-       document.getElementById("image").innerHTML = "Hover over an image below to display here.";
-           
-       }
+function billingFunction(){
+  
+    if (document.getElementById('same').checked){
+      var name=document.getElementById('shippingName').value;
+      var zip=document.getElementById('shippingZip').value;
+      
+      document.getElementById('billingName').value = name; 
+      document.getElementById('billingZip').value = zip;
+      
+    }else{
+      document.getElementById('billingName').value = '';
+      document.getElementById('billingZip').value =  '';
+    }
+  
+}
